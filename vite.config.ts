7352fa161ea823711e904d7f58af424e3e41c50a
virtu0427 <jdi04278@gmail.com>
2024-11-21 23:20:57 +0900

    Revert "배포 위한 vite 설정"
    This reverts commit c59476d7d935113aeef8cf06c0a606b916d1090a.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/web2project/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
