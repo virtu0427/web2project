64413d9af8b2addf8ccbf124430a8f10d2cb77cc
virtu0427 <jdi04278@gmail.com>
2024-11-25 15:14:38 +0900

    base url 수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/web2project/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
